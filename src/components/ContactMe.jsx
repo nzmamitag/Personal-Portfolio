c32332b74424446d9cd40632af1c0bd96f7c9497
Neds Mamitag <nzmamitag@gmail.com>
2024-05-10 23:32:24 +0800

    Initial Commit

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -15,16 +15,18 @@ function ContactMe() {
     }, []);
 
     const downloadResume = (url) => {
-        fetch(url).then(response.response.blob()).then(blob => {
-            const blobURL = window.URL.createObjectURL(new Blob([blob]))
-            const fileName = url.split('/').pop();
-            const aTag = document.createElement('a');
-            aTag.href = blobURL;
-            aTag.setAttribute('download', fileName);
-            document.body.appendChild(aTag);
-            aTag.click();
-            aTag.remove();
-        })
+        fetch(url)
+            .then((response) => response.blob())
+            .then((blob) => {
+                const blobURL = window.URL.createObjectURL(new Blob([blob]))
+                const fileName = url.split('/').pop();
+                const aTag = document.createElement('a');
+                aTag.href = blobURL;
+                aTag.setAttribute('download', fileName);
+                document.body.appendChild(aTag);
+                aTag.click();
+                aTag.remove();
+            })
     }
 
     return (
